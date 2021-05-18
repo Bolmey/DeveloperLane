64f59812b0dc0e5908e380e54fa71197218a34f5
Juan Bolmey <72952859+Bolmey@users.noreply.github.com>
2021-05-18 11:21:36 -0400

    CSS

--- client/src/components/monitor/Monitor.js
+++ client/src/components/monitor/Monitor.js
@@ -9,11 +9,53 @@ function Monitor(props) {
     const [mainProduct, setMainProduct] = useState([])
     const [otherProducts, setOtherProducts] = useState([])
 
+    let other = [
+        {
+            name: "Samsung LC49G95TSSNXZA",
+            image: "https://robbreport.com/wp-content/uploads/2020/01/g901.jpg?w=1000",
+            description
+                :
+                "Get your head in the game with the 49 inch Odyssey G9, which matches t...",
+            price
+                :
+                1399.99,
+            rating
+                :
+                "4.7",
+            best
+                :
+                false
+        },
+        {
+            name
+                :
+                "ViewSonic Elite XG270QG",
+            image
+                :
+                "https://www.techtoyreviews.com/wp-content/uploads/2020/06/ViewSonic-mo...",
+            description
+                :
+                "The ViewSonic Elite XG270QG is a good monitor for most uses. It perfor...",
+            price
+                :
+                559.99,
+            rating
+                :
+                "4.5",
+            best
+                :
+                false
+        }
+    ]
+
     useEffect(() => {
         actions.getMainMonitor()
             .then(async (res) => await setMainProduct(res[0]))
         actions.getOtherMonitors()
-            .then(async (res) => await setOtherProducts(res))
+            .then(async (res) => {
+                await setOtherProducts(res)
+                setOtherProducts(other)
+            })
     }, [])
 
 
@@ -41,7 +83,7 @@ function Monitor(props) {
                                     <h2>${product.price}</h2>
                                     <button className="button" type="button">Add to Cart</button>
                                 </div>
-                            </div>
+                            </div>ç
                         </div>
                     </div>
                 )
@@ -56,7 +98,10 @@ function Monitor(props) {
             <div className="main-product-container">
                 <div className="heading"></div>
                 <div className="main-product-img">
-                    <img src={mainProduct.image} />
+                    <div className="main-picture" style={{ backgroundImage: `url(${mainProduct.image})`, backgroundPosition: `center`, backgroundSize: '100%' }}>
+
+                    </div>
+                    {/* <img src={mainProduct.image} /> */}
                 </div>
                 <div className="main-details-container">
                     <div className="main-text-container">
@@ -75,10 +120,10 @@ function Monitor(props) {
             </div>
 
             <div className="productContainer">
-                {/* {displayOtherProducts()} */}
+                {displayOtherProducts()}
             </div>
 
-        </div>
+        </div >
     )
 }
 
